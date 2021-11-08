e30404e01d0cd520ac7e81d549eb6e8beedbb412
agoyal1981 <arungoyal@me.com>
2021-11-08 01:07:33 +0000

    Change code to check for errors

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -3,25 +3,33 @@ import java.util.Scanner;
 class Dec2Hex
 {
 
-    public static int Arg1;
+    //public static int Arg1;
 
-    public static void main(String args[])
+    public static void main(String args[])throws IOException
     {
-        Arg1 = Integer.parseInt(args[0]);
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	System.out.println("Enter a Decimal Number: ");
+	int n = Integer.parseInt(br.readLine());
+	int r;
+	String s = "";
+        //Arg1 = Integer.parseInt(args[0]);
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-        int rem, num;
-        num = Arg1;
-        String hexadecimal="";
-        System.out.println("Converting the Decimal Value " + num + " to Hex...");
+	//Scanner in = new Scanner(System.in);//take user input        
+	//int rem, num;
+        //num = Arg1;
+	//System.out.print("Input a number: ");
+	//dnum = in.nextInt();
+        //String hexadecimal="";
+        //System.out.println("Converting the Decimal Value " + dnum + " to Hex...");
 
-        while(num != 0)
+        while(n > 0)
         {
-            rem=num%16;
-            hexadecimal= ch[rem] + hexadecimal;
-            num= num/16;
+            r = n%16;
+            s = ch[r] + s;
+            n= n/16;
         }
 
-        System.out.println("Hexadecimal representation is: " + hexadecimal);
+        System.out.println("Hexadecimal representation is: " + s);
 
     }
 }
