c05698bd706e401f565e20b427772e2c16f2c3ab
agoyal1981 <arungoyal@me.com>
2021-11-10 10:41:04 +0000

    Code changed for Error Handling or No Input

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -3,14 +3,33 @@ import java.io.*;
 
 class Dec2Hex
 {
+	public static int Arg = 0;
 
     public static void main(String args[])
     {
+	// return an error if no input argument is provided 
+        if(args.length == 0) 
+	{
+           System.out.println("Please Enter a Decimal Number to Convert");
+           return;
+        }
+
+       // error handling to ensure that non-Integer inputs are not converted
+	// also do not result in a failed execution
+       
+        try
+        {
+             Arg1 = Integer.parseInt(args[0]);
+        }
+        catch(NumberFormatException ex)
+        {
+          System.out.println("Please Enter Valid Decimal Number: ");
+          return;
+        }
+
 	System.out.println("Conversion of Decimal to Hexadecimal");
-	Scanner scan = new Scanner(System.in);
-	System.out.println("Enter a Decimal Number: ");
-	int num = scan.nextInt();
-	int rem;
+	int num, rem;
+	int num = Arg1;
 	String hex = "";
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
 	
